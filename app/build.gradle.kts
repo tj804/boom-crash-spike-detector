d89ea90ad027187cc13d4c27fa03cbbbde7d6f00
tj804 <tabjim70@gmail.com>
2026-09-23 16:30:52 +0300

    Update Java and Kotlin compatibility to version 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,10 +14,19 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlinOptions {
+    jvmTarget = "17"
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-}
+} look
