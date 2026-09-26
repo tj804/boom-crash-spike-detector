da051c5224b07d1f5dd385e5e1db4f0419c6057d
tj804 <tabjim70@gmail.com>
2026-09-26 12:32:56 +0300

    Update versionCode and versionName in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,15 @@ android {
         applicationId = "com.tj804.boomcrashdetector"
         minSdk = 23
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = false
+        }
     }
 
     compileOptions {
